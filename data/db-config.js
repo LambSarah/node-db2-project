e20806e2c9805f71cc0fd10d9fa5e81da7267e45
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-25 23:59:02 -0500

    db created

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,7 @@
 // no need to change this file
 const knex = require('knex')
 const configs = require('../knexfile.js')
+// eslint-disable-next-line no-undef
 const environment = process.env.NODE_ENV || 'development'
 
 module.exports = knex(configs[environment])
